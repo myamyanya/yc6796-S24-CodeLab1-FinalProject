edc20acd94314c6f155b7318c1e9b19b066a7cf3
VivianC <37090776+myamyanya@users.noreply.github.com>
2024-04-27 01:07:04 -0400

    Add dialogues to the project

---
+++ Final-ThisIsMyFinalProject/Assets/Scripts/NPCLogicNeutral.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCLogicNeutral : NPCLogic
+{
+    public override void NPCBehave()
+    {
+        base.NPCBehave();
+    }
+}
